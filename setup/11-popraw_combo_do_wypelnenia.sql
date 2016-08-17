badec2492412f4f29735c1545f5246f86ec28a89
Kamil Jakacki <kamil@zapytania.com>
2016-08-17 13:13:56 +0200

    wypelnia wlasciciel

---
+++ setup/11-popraw_combo_do_wypelnenia.sql
@@ -0,0 +1,3 @@
+update userzasoby 
+set modul = replace(modul, "do wypełnienia przez administratora zasobu", "do wypełnienia przez właściciela zasobu"),
+ poziomDostepu = replace(poziomDostepu, "do wypełnienia przez administratora zasobu", "do wypełnienia przez właściciela zasobu");
