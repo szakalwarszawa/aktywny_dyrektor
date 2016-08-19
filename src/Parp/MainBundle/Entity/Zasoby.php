840b84b916bf3bd5bc93b161cbf605ca6091838b
Kamil Jakacki <kamil@zapytania.com>
2016-08-19 11:52:41 +0200

    f2

--- src/Parp/MainBundle/Entity/Zasoby.php
+++ src/Parp/MainBundle/Entity/Zasoby.php
@@ -174,21 +174,21 @@ class Zasoby
     /**
     * @var string
      *
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      * @Gedmo\Mapping\Annotation\Versioned
      */
     private $modulFunkcja;
     /**
     * @var string
      *
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      * @Gedmo\Mapping\Annotation\Versioned
      */
     private $poziomDostepu;
     /**
     * @var string
      *
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      * @Gedmo\Mapping\Annotation\Versioned
      */
     private $grupyAD;
