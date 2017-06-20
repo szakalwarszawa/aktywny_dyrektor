7c6fa99b0afff8bbaa45d4c416dc864f04cb1993
Robert Muchacki <robert_muchacki@parp.gov.pl>
2017-06-20 15:03:01 +0200

    Dodanie możliwość logowania się na dowolnego użytkownika w środowisku o identyfikatorze 'test'

--- src/Parp/AuthBundle/Security/ParpUserProvider.php
+++ src/Parp/AuthBundle/Security/ParpUserProvider.php
@@ -3,105 +3,110 @@
 namespace Parp\AuthBundle\Security;
 
 use Symfony\Component\Config\Definition\Exception\Exception;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class ParpUserProvider implements UserProviderInterface
 {
-    var $ad_host = "";
-    var $ad_domain = "";
-    var $ad_ou = "";
-    var $ad_dc1 = "";
-    var $ad_dc2 = "";
-
+    protected $ad_host = '';
+    protected $ad_domain = '';
+    protected $ad_ou = '';
+    protected $ad_dc1 = '';
+    protected $ad_dc2 = '';
+    protected $idSrodowiska;
+    protected $kernel;
 
     public function __construct()
     {
         global $kernel;
 
-        if ('AppCache' == get_class($kernel)) {
-            $kernel = $kernel->getKernel();
+        /** @var \AppKernel $kernel */
+        if ('AppCache' === get_class($kernel)) {
+            $this->kernel = $kernel->getKernel();
         }
 
-
-        $tab = explode(".", $kernel->getContainer()->getParameter('ad_domain'));
+        $tab = explode('.', $kernel->getContainer()->getParameter('ad_domain'));
         $this->ad_host = $kernel->getContainer()->getParameter('ad_host');
         $this->ad_domain = $kernel->getContainer()->getParameter('ad_domain');
         $this->ad_ou = $kernel->getContainer()->getParameter('ad_ou');
         $this->ad_dc1 = $tab[0];
         $this->ad_dc2 = $tab[1];
-        //die( ".". $this->ad_host.".".$this->ad_domain.".".$this->ad_ou.".");
-        //die('a');
+        $this->idSrodowiska = $kernel->getContainer()->getParameter('id_srodowiska');
     }
 
     public function loadUserByUsername($username)
     {
         global $kernel;
-        $ldapconn = ldap_connect($this->ad_host);
-        $ldapdomain = "@".$this->ad_domain;//parp.local";
-         if($_POST){
+
+        $idSrodowiska = $this->idSrodowiska;
+        $ldapconn = null;
+
+        if ('test' !== $idSrodowiska) {
+            $ldapconn = ldap_connect($this->ad_host);
+        }
+
+        $ldapdomain = '@'.$this->ad_domain;//parp.local";
+        if ($_POST) {
             $username = $_POST['_username'];
             $password = $_POST['_password'];
-        }
 
-            //die( ".11.".$username.$ldapdomain);
-      //var_dump($username);
-      //die();
-
-//      DEBUG ONLY:
-//      $password = null;
-//      $salt = null;
-//      $roles = array('ROLE_USER');
-//      ldap_unbind($ldapconn);
-
-//      return new ParpUser($username, $password, $salt, $roles);
-
-        if ($ldapconn) {
-            try {
-                    //die( ".".$username.".".$ldapdomain.".".$password.".");
-                //var_dump($_SERVER);
-                //die();
-                if($_SERVER['HTTP_HOST'] == 'localhost:15552'){
-                    $ldapbind = true;
-                }else {
-                    $ldapbind = ldap_bind($ldapconn, $username . $ldapdomain, $password);
-                }
-            } catch (Exception $e) {
-                //die('.1');
-                throw new UsernameNotFoundException(sprintf('Użytkownik "%s" nie istnieje.', $username));
-            }
-
-            if ($ldapbind) {
-                //echo ".12.";
+            if ('test' === $idSrodowiska) {
+                $password = 'test';
                 $salt = null;
-
-                $rolesEntities = [];
-                //temp Grzesia wniosek
-                //echo "!!!!pobieram role $username !!!";
-                $rolesEntities = $kernel->getContainer()->get('doctrine')->getRepository('ParpMainBundle:AclUserRole')->findBySamaccountname($username);
-
-                $roles = [];
-                foreach($rolesEntities as $r){
-                    $roles[] = $r->getRole()->getName();
-                }
-                //print_r($roles);
-                //die($username);
-                //$roles = array('ROLE_USER');
-                ldap_unbind($ldapconn);
+                $roles = array(
+                    'ROLE_USER',
+                    'PARP_MANAGER',
+                    'PARP_BZK_1',
+                    'PARP_BZK_2',
+                    'PARP_ADMIN',
+                    'PARP_ADMIN_ZASOBOW',
+                    'PARP_ADMIN_TECHNICZNY_ZASOBOW'
+                );
 
                 return new ParpUser($username, $password, $salt, $roles);
-            } else {
-                //die('.2'.$this->ad_host);
+            }
+
+            if ($ldapconn) {
+                try {
+                    if ('test' === $idSrodowiska) {
+                        $ldapbind = true;
+                    } else {
+                        $ldapbind = ldap_bind($ldapconn, $username.$ldapdomain, $password);
+                    }
+                } catch (Exception $e) {
+                    throw new UsernameNotFoundException(sprintf('Użytkownik "%s" nie istnieje.', $username));
+                }
+
+                if ($ldapbind) {
+                    $salt = null;
+                    $rolesEntities =
+                        $kernel->getContainer()
+                            ->get('doctrine')
+                            ->getRepository('ParpMainBundle:AclUserRole')
+                            ->findBy([
+                                'samaccountname' => $username
+                            ])
+                        ;
+
+                    $roles = [];
+                    /** @var array $rolesEntities */
+                    foreach ($rolesEntities as $r) {
+                        $roles[] = $r->getRole()->getName();
+                    }
+                    ldap_unbind($ldapconn);
+
+                    return new ParpUser($username, $password, $salt, $roles);
+                }
                 throw new UsernameNotFoundException(sprintf('Użytkownik "%s" nie istnieje.', $username));
             }
 
-        }else{
-            die('ldapconn not valid');
+            throw new UsernameNotFoundException(sprintf('Użytkownik "%s" nie istnieje.', $username));
         }
-                //die('.3');
-        throw new UsernameNotFoundException(sprintf('Użytkownik "%s" nie istnieje.', $username));
+
+        throw new MethodNotAllowedException(['POST']);
     }
 
     public function refreshUser(UserInterface $user)
@@ -109,17 +114,20 @@ class ParpUserProvider implements UserProviderInterface
         if (!$user instanceof ParpUser) {
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
         }
-        if($this->auth($user->getUsername(),$user->getPassword()))
+
+        if ($this->auth($user->getUsername(), $user->getPassword())) {
             return new ParpUser($user->getUsername(), $user->getPassword(), $user->getSalt(), $user->getRoles());
-        else
-            throw new UsernameNotFoundException(sprintf('Użytkownik "%s" nie istnieje.', $user->getUsername()));
+        }
+
+        throw new UsernameNotFoundException(sprintf('Użytkownik "%s" nie istnieje.', $user->getUsername()));
     }
 
-    public function auth($ldapuser,$ldappass){
-        //die( ".auth.");
+    public function auth($ldapuser, $ldappass)
+    {
+        $idSrodowiska = $this->idSrodowiska;
         $ldapconn = ldap_connect($this->ad_host);
-        $ldapdomain = "@".$this->ad_domain;
-        $userdn = $this->ad_ou.",DC=parp,DC=local";
+        $ldapdomain = '@'.$this->ad_domain;
+        $userdn = $this->ad_ou.',DC=parp,DC=local';
         ldap_set_option($ldapconn, LDAP_OPT_SIZELIMIT, 2000);
 
         ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3) or die('Unable to set LDAP protocol version');
@@ -127,27 +135,26 @@ class ParpUserProvider implements UserProviderInterface
 
         if ($ldapconn &&
             $ldappass &&
-            $ldapuser)
-        {
-
-                try {
-
-                    //die( ".".$ldapuser.".".$ldapdomain.".".$ldappass.".");
-                    if($_SERVER['HTTP_HOST'] == 'localhost:15552'){
-                        $ldapbind = true;
-                    }else {
-                        $ldapbind = ldap_bind($ldapconn, $ldapuser . $ldapdomain, $ldappass) or die('Nieprawidłowe dane!');
-                    }
-                } catch(Exception $e) {
-                    throw new Exception('Brak komunikacji z serwerem!');
+            $ldapuser
+        ) {
+            try {
+                if ('test' === $idSrodowiska) {
+                    $ldapbind = true;
+                } else {
+                    $ldapbind = ldap_bind($ldapconn, $ldapuser.$ldapdomain, $ldappass) or die('Nieprawidłowe dane!');
                 }
+            } catch (Exception $e) {
+                throw new Exception('Brak komunikacji z serwerem!');
+            }
 
 
             if ($ldapbind) {
                 ldap_unbind($ldapconn);
-                    return true;
+
+                return true;
             }
         }
+
         return false;
     }
 
@@ -157,5 +164,3 @@ class ParpUserProvider implements UserProviderInterface
         return $class === 'Parp\AuthBundle\Security\ParpUser';
     }
 }
-
-?>
