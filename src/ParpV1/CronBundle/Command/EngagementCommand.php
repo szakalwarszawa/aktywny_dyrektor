568fdcb900085ee3f30706c27354c0fb0f2bcba4
Tomasz Bończak <tomasz_bonczak@parp.gov.pl>
2017-11-28 15:55:15 +0100

    Paweł commit-uje śmietnik przed urlopem

--- src/ParpV1/CronBundle/Command/EngagementCommand.php
+++ src/ParpV1/CronBundle/Command/EngagementCommand.php
@@ -26,19 +26,56 @@ class EngagementCommand extends ContainerAwareCommand
 
         $userEngagements = $em->getRepository('ParpMainBundle:UserEngagement')->findByCzyNowy(true);
 
-        $xx = $ldap->getPrzelozonyPracownika('tomasz_bonczak');
+        $adresyDo = [];
 
-        // sprawdz czy ma zastepstwo
-        $zastepstwo = $em->getRepository('ParpMainBundle:Zastepstwo')->znajdzZastepstwa($xx['samaccountname']);
+        // dodaj pracowników grup HelpDesk BI, KOMP
+        $grupaHelpdesk = $em->getRepository('ParpMainBundle:AclRole')->findOneByName('PARP_HELPDESK_BI');
+        if (!empty($grupaHelpdesk->getUsers())) {
+            foreach($grupaHelpdesk->getUsers() as $user){
+                $samaccountname= $user->getSamaccountname();
+                if(!in_array($samaccountname, $adresyDo)){
+                    $adresyDo[] = $samaccountname;
+                }
+            }
+        }
 
-        var_dump(empty($zastepstwo));
+        $grupaKomp = $em->getRepository('ParpMainBundle:AclRole')->findOneByName('PARP_KOMP');
+        if (!empty($grupaKomp->getUsers())) {
+            foreach($grupaKomp->getUsers() as $user){
+                $samaccountname= $user->getSamaccountname();
+                if(!in_array($samaccountname, $adresyDo)){
+                    $adresyDo[] = $samaccountname;
+                }
+            }
+        }
+
+        $powiernicy = [];
+        $grupaPowiernikZarzadu = $em->getRepository('ParpMainBundle:AclRole')->findOneByName('PARP_KOMP');
+        if (!empty($grupaPowiernikZarzadu->getUsers())) {
+            foreach ($grupaPowiernikZarzadu->getUsers() as $user) {
+                $samaccountname = $user->getSamaccountname();
+                if (!in_array($samaccountname, $adresyDo)) {
+                    $powiernicy[] = $samaccountname;
+                }
+            }
+        }
+
+        var_dump($powiernicy);
+die();
+       ///var_dump(empty($zastepstwo));
         // jezeli nie dyrektor to dodaj adres Pani(Przerobic na grupę) - POWIERNIK_ZARZADU
 
         // dodaj pracowników grup HelpDesk BI, KOMP
 
         // wyslij maila - nowa metoda w usłudze parp.mailer chyba
 
-        var_dump($xx['samaccountname']);
+        //var_dump($xx['samaccountname']);
+        //
+        //
+        //$xx = $ldap->getPrzelozonyPracownika('tomasz_bonczak');
+        //$xx = $ldap->getPrzelozonyPracownika('tomasz_bonczak');
+        // sprawdz czy ma zastepstwo
+        //$zastepstwo = $em->getRepository('ParpMainBundle:Zastepstwo')->znajdzZastepstwa($xx['samaccountname']);
 
         if (!empty($userEngagements)) {
             foreach ($userEngagements as $userEngagement) {
@@ -49,12 +86,11 @@ class EngagementCommand extends ContainerAwareCommand
 
                 $today = new \DateTime();
 
-                if($date <= $today){
-                       $user = $ldap->getUserFromAD($userEngagement->getSamaccountname());
-                       //$user = $users[$userEngagement->getSamaccountname()];
-                       var_dump($user);
+                if ($date <= $today) {
+                    $user = $ldap->getUserFromAD($userEngagement->getSamaccountname());
+                    //$user = $users[$userEngagement->getSamaccountname()];
+                    var_dump($user);
                 }
-
             }
         }
     }
