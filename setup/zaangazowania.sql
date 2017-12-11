447f4a94b30a8acba5fc4dd4359058dbe56d1f2c
Paweł Fedoruk <pawel_fedoruk@parp.gov.pl>
2017-12-11 16:04:27 +0100

    dodanie roli PARP_IMPORT_ZAANGAZOWANIE

--- setup/zaangazowania.sql
+++ setup/zaangazowania.sql
@@ -4,3 +4,4 @@ ALTER TABLE userengagement CHANGE percent percent INT DEFAULT NULL;
 INSERT INTO `acl_role` (`name`, `opis`) VALUES ('PARP_HELPDESK_BI', 'helpdesk Biura Informatyki');
 INSERT INTO `acl_role` (`name`, `opis`) VALUES ('PARP_KOMP', 'rola związana z procesem wydawania i odbierania sprzętu komputerowego');
 INSERT INTO `acl_role` (`name`, `opis`) VALUES ('PARP_POWIERNIK_ZARZADU', 'otrzymuje maile o zmianie zaangażowania zamiast zarządu');
+INSERT INTO `acl_role` (`name`, `opis`) VALUES ('PARP_IMPORT_ZAANGAZOWANIE', 'może zaimportować plik z zaangażowaniami');
