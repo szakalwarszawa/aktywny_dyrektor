5563efbd656b482f53de7b2fb7a5f8dc2b905498
luklub <luklub@o2.pl>
2018-01-25 16:31:26 +0100

    Poprawiona literówka w nazwie InvalidContentException.

--- src/ParpV1/MainBundle/Api/Exception/InvalidContentException.php
+++ src/ParpV1/MainBundle/Api/Exception/InvalidContentException.php
@@ -8,6 +8,6 @@ use ParpV1\MainBundle\Api\Exception\ApiException;
  * Wyjątek rzucany jeśli np. przygotowana treść odpowiedzi nie odpowiada pod kątem
  * wartości i/lub struktury założeniem (np. brak wymaganych wartości).
  */
-class InvalidContentExcpetion extends ApiException
+class InvalidContentException extends ApiException
 {
 }
