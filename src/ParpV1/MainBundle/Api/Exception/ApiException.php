83a84dfb87ed690c1c8f38a107d5ea36bca4f23a
luklub <luklub@o2.pl>
2018-01-25 16:35:12 +0100

    Poprawki w adnotacjach i doc-block.

--- src/ParpV1/MainBundle/Api/Exception/ApiException.php
+++ src/ParpV1/MainBundle/Api/Exception/ApiException.php
@@ -4,6 +4,9 @@ namespace ParpV1\MainBundle\Api\Exception;
 
 /**
  * Podstawowy wyjątek dla operacji powiązanych z API.
+ *
+ * Ten wyjątek zazwyczaj nie powinien być rzucany bezpośrednia.
+ * Jego celem jest identyfikacja wyjątków dziedziczących jako powiązanych z API.
  */
 class ApiException extends \Exception
 {
--- src/ParpV1/MainBundle/Api/Exception/InvalidContentException.php
+++ src/ParpV1/MainBundle/Api/Exception/InvalidContentException.php
@@ -5,8 +5,10 @@ namespace ParpV1\MainBundle\Api\Exception;
 use ParpV1\MainBundle\Api\Exception\ApiException;
 
 /**
- * Wyjątek rzucany jeśli np. przygotowana treść odpowiedzi nie odpowiada pod kątem
- * wartości i/lub struktury założeniem (np. brak wymaganych wartości).
+ * Wyjątek rzucany jeśli zawartość (obiektu, tablicy, JSON, itp.) nie odpowiada pod kątem
+ * wartości i/lub struktury założeniem (np. brak wymaganych wartości w obiekcie odpowiedzi JSON).
+ *
+ * Ten wyjątek najcześciej powoduje zwrotną odpowiedź z API o statusue 422 Unprocessabel Entity.
  */
 class InvalidContentException extends ApiException
 {
