fbfd0a13a5ce030cbd470b01c8c1026349d74b61
Paweł Fedoruk <pawel_fedoruk@parp.gov.pl>
2019-02-18 11:22:13 +0100

    Update Version20190218100714.php

--- setup/migrations/Version20190218100714.php
+++ setup/migrations/Version20190218100714.php
@@ -14,7 +14,7 @@ final class Version20190218100714 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('UPDATE userzasoby SET aktywneDo = DATE_FORMAT(aktywneDo, "%Y-%m-%d 23:59:00")');
+        $this->addSql('UPDATE userzasoby SET aktywneDo = DATE_FORMAT(aktywneDo, "%Y-%m-%d 23:59:00") WHERE TIME(aktywneDo) = "00:00:00"');
     }
 
     public function down(Schema $schema) : void
