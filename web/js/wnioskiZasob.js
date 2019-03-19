89d11028748b9e51ae95096a3ee4da26ac29ec66
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-03-19 11:45:12 +0100

    #75684 commituje bo musze

--- web/js/wnioskiZasob.js
+++ web/js/wnioskiZasob.js
@@ -71,7 +71,7 @@ $(document).ready(function () {
 	//-----------------------------
 	var submitBottom = $('#parp_mainbundle_wniosekutworzeniezasobu_submit');
 	var submitTop = $('#parp_mainbundle_wniosekutworzeniezasobu_submit2');
-
+	console.log("---where is my script---");
 
 	var inputWlascicielZasobu = $('#parp_mainbundle_wniosekutworzeniezasobu_zasob_wlascicielZasobu');
 	console.log(inputWlascicielZasobu);
