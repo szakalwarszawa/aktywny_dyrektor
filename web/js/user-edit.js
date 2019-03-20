e6b63d293896d4b040fc31ba3e38a46a361b80f9
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-03-20 15:15:16 +0100

    #75955 usuniecie zbednego offsetu dla xs

--- web/js/user-edit.js
+++ web/js/user-edit.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
 	var kontoSelect = $('#parp_mainbundle_edycjauzytkownika_isDisabled');
 	var hiddenReason = $('#powod_wylaczenia_w_ad');
 	$(kontoSelect).on('change', function () {
-		if ($(kontoSelect).val() === "1") {
+		if ($(kontoSelect).val() === '1') {
 			$(hiddenReason).removeClass('hidden');
-		} else if ($(kontoSelect).val() === "0" && $(hiddenReason).hasClass('hidden') === false) {
+		} else if ($(kontoSelect).val() === '0' && $(hiddenReason).hasClass('hidden') === false) {
 			$(hiddenReason).addClass('hidden');
 		} else {
 			return;
