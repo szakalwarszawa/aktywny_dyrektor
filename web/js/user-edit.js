b3f0331054152053302a9ec15fd53cb261088b06
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-03-20 13:09:07 +0100

    #75955 wyrownanie

--- web/js/user-edit.js
+++ web/js/user-edit.js
@@ -2,4 +2,7 @@ console.log('USER EDIT');
 
 $(document).ready(function () {
 	var kontoSelect = $('#parp_mainbundle_edycjauzytkownika_isDisabled');
+	$(kontoSelect).on('change', function () {
+		console.log("Zmiana");
+	});
 });
