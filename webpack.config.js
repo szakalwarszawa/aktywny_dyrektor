cd22cf258276cc486ebcdfd15a2ab79a086d7b94
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-04-01 15:11:18 +0200

    feature/76993 yarn & encor test

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    .setOutputPath('web/build/')
-    .setPublicPath('/build')
-    .addEntry('app', './web/assets/js/app.js')
-    .enableSingleRuntimeChunk()
-    .cleanupOutputBeforeBuild()
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
-    .splitEntryChunks()
-    //.autoProvidejQuery()
-;
+	.setOutputPath('web/build/')
+	.setPublicPath('/build')
+	.addEntry('app', './web/assets/js/app.js')
+	.enableSingleRuntimeChunk()
+	.cleanupOutputBeforeBuild()
+	.enableSourceMaps(!Encore.isProduction())
+	.enableVersioning(Encore.isProduction())
+	.splitEntryChunks()
+	//.autoProvidejQuery()
+	.enableSassLoader();
 
 module.exports = Encore.getWebpackConfig();
