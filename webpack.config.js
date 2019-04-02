69d1f7d628b8a65d74d408362b16b69f2c0d4e89
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-04-02 13:58:32 +0200

    #77431 2x development

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
+// var Encore = require('@symfony/webpack-encore');
+
+// Encore
+// 	.setOutputPath('web/build/')
+// 	.setPublicPath('/build')
+// 	.cleanupOutputBeforeBuild() //new
+// 	.addEntry('app', './web/assets/js/app.js')
+// 	.enableSingleRuntimeChunk()
+// 	.cleanupOutputBeforeBuild()
+// 	.enableSourceMaps(!Encore.isProduction())
+// 	.enableVersioning(Encore.isProduction())
+// 	.splitEntryChunks()
+// 	.autoProvidejQuery()
+// 	.enableSassLoader();
+
+// module.exports = Encore.getWebpackConfig();
+
+
 var Encore = require('@symfony/webpack-encore');
 
 Encore
 	.setOutputPath('web/build/')
 	.setPublicPath('/build')
+	.cleanupOutputBeforeBuild() //new
 	.addEntry('app', './web/assets/js/app.js')
 	.enableSingleRuntimeChunk()
 	.cleanupOutputBeforeBuild()
@@ -12,4 +31,16 @@ Encore
 	.autoProvidejQuery()
 	.enableSassLoader();
 
-module.exports = Encore.getWebpackConfig();
+var config = Encore.getWebpackConfig();
+
+config.devServer.watchOptions = {
+	poll: true,
+	ignored: /node_modules/,
+	inline: true,
+	contentBase: './',
+	port: 8000,
+	historyApiFallback: true
+};
+
+
+module.exports = config;
