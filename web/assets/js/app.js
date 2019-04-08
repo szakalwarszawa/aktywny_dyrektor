bb9dd5b87022b9b923ff78fa2482c82f02923fa1
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-04-08 15:24:35 +0200

    #77431 typo in composer.json

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -9,7 +9,7 @@ const tab = [1, 2, 7, 4, 5];
 for (let i = 0; i < 9; i++) {
 	if (tab.includes(7)) {
 		let sorted = tab.map(item => item * 2);
-		console.log("Babel test");
+		console.log("Babel test: ", sorted);
 	} else {
 		(() => {
 			class Prostokat {
