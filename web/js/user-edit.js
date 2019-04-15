c339d6d182d9841bd83aed0404a65766b9b94e07
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-04-15 14:24:06 +0200

    76335 select value test

--- web/js/user-edit.js
+++ web/js/user-edit.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 	var kontoSelect = $('#parp_mainbundle_edycjauzytkownika_isDisabled');
 	var hiddenReason = $('#powod_wylaczenia_w_ad');
 	$(kontoSelect).on('change', function () {
+		console.log($(kontoSelect).val());
 		if ($(kontoSelect).val() === '1') {
 			$(hiddenReason).removeClass('hidden');
 		} else if ($(kontoSelect).val() === '0' && $(hiddenReason).hasClass('has-error') === false && $(hiddenReason).hasClass('hidden') === false) {
