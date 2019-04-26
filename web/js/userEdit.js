8709590ea9822d8ab3d12b06307ca1bf078a452f
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-04-26 13:14:40 +0200

    #76335 select2 matcher template

--- web/js/userEdit.js
+++ web/js/userEdit.js
@@ -68,27 +68,41 @@ $(document).ready(function () {
 
 	// console.log('group name ', optgroupsTxt);
 
+	// --- własny matcher do select2 ---
+	function matchCustom(params, data) {
+		// If there are no search terms, return all of the data
+		if ($.trim(params.term) === '') {
+			return data;
+		}
+
+		// Do not display the item if there is no 'text' property
+		if (typeof data.text === 'undefined') {
+			return null;
+		}
+
+		// `params.term` should be the term that is used for searching
+		// `data.text` is the text that is displayed for the data object
+		if (data.text.indexOf(params.term) > -1) {
+			var modifiedData = $.extend({}, data, true);
+			modifiedData.text += ' (matched)';
+
+			// You can return modified objects from here
+			// This includes matching the `children` how you want in nested data sets
+			return modifiedData;
+		}
+
+		// Return `null` if the term should not be displayed
+		return null;
+	}
+
 	// --- uruchomienie biblioteki select2 na wybranych selectach ---
-	$(sekcja).select2(); // deaktywacja select2 sekcji na czas obróbki danych
+	$(sekcja).select2({
+		matcher: matchCustom // funkcja definiująca dopasownia przy wyszukiwaniu za pomocą select2
+	});
 	$(menager).select2();
-	// --- zmienne zależne od wykonania select2 ---
-	// var sekcjaResults = $('#select2-parp_mainbundle_edycjauzytkownika_info-results');
 
-	// --- zaciąganie elementów po podanym czasie ---
-	// var timeout = function (time) {
-	// 	window.setTimeout(modifySelect2Results(), time)
-	// };
 
-	// function modifySelect2Results() {
-	// 	var sekcjaResults = document.getElementById('select2-parp_mainbundle_edycjauzytkownika_info-results');
-	// 	console.log(sekcjaResults);
-	// 	var sekcjaResultsItem = $(sekcjaResults).children('li');
-	// 	console.log('item', sekcjaResultsItem);
-	// 	console.log('item2', $(sekcjaResults).find('li'));
-	// 	clearTimeout(timeout);
-	// }
 
-	// timeout(20000);
 
 	if ($(kontoSelect).val() === '1') {
 		$(hiddenReason).removeClass('hidden');
