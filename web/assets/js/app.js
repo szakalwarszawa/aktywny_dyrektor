61c43c5ea7681efd0889008b6f41b6692d1af52b
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-05-07 10:37:56 +0200

    #78346 changing popover max-width

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -2,7 +2,7 @@ require('../scss/app.scss');
 
 // var $ = require('jquery');
 
-console.info("%c Hello from Encore", "text-shadow: 4px 3px 0 #7A7A7A; color: #fff; font-size: 14px;");
+console.info("%c Hello from Encore", "text-shadow: 2px 1px 0 #7A7A7A; color: #fff; font-size: 18px;");
 
 // const tab = [1, 2, 6, 4, 5];
 
