c5073b19fe415891d14477c03e3d67b7b8ebf274
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-05-07 10:28:31 +0200

    #78346 old look of navbar _custom_variables.scss white links

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -2,31 +2,31 @@ require('../scss/app.scss');
 
 // var $ = require('jquery');
 
-console.log("Hello from Encore");
+console.info("%c Hello from Encore", "text-shadow: 4px 3px 0 #7A7A7A; color: #fff; font-size: 14px;");
 
-const tab = [1, 2, 6, 4, 5];
+// const tab = [1, 2, 6, 4, 5];
 
-for (let i = 0, sorted; i < 9; i++) {
-	let width = 0;
-	if (tab.includes(7)) {
-		(() => {
-			sorted = tab.map(item => item * 2 + i);
-			console.log(i, "Babel test: ", sorted);
-			return sorted;
-		})();
-	} else {
-		(() => {
-			class Prostokat {
-				constructor(wysokosc, szerokosc) {
-					this.wysokosc = wysokosc;
-					this.szerokosc = szerokosc;
-				}
-			}
-			const p = new Prostokat(5, 77);
-			let width = p.szerokosc;
-			return width;
-		})();
-	}
+// for (let i = 0, sorted; i < 9; i++) {
+// 	let width = 0;
+// 	if (tab.includes(7)) {
+// 		(() => {
+// 			sorted = tab.map(item => item * 2 + i);
+// 			console.log(i, "Babel test: ", sorted);
+// 			return sorted;
+// 		})();
+// 	} else {
+// 		(() => {
+// 			class Prostokat {
+// 				constructor(wysokosc, szerokosc) {
+// 					this.wysokosc = wysokosc;
+// 					this.szerokosc = szerokosc;
+// 				}
+// 			}
+// 			const p = new Prostokat(5, 77);
+// 			let width = p.szerokosc;
+// 			return width;
+// 		})();
+// 	}
 
-	console.info(`${i} sorting list ${sorted} o szerokości ${width}`);
-}
+// 	console.info(`${i} sorting list ${sorted} o szerokości ${width}`);
+// }
