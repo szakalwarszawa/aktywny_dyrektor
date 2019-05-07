64d3f64719e7f6e1eb1899d98ac885a0712d5b6c
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-05-07 13:54:36 +0200

    #78346 100% width for filters on grid

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -2,7 +2,7 @@ require('../scss/app.scss');
 
 // var $ = require('jquery');
 
-console.info("%c Hello from Encore", "text-shadow: 2px 1px 0 #7A7A7A; color: #fff; font-size: 18px;");
+console.info("%c Hello from Encore ", "text-shadow: 2px 1px 0 #7A7A7A; color: #fff; font-size: 18px;");
 
 // const tab = [1, 2, 6, 4, 5];
 
