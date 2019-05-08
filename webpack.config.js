13c6973c7907237dc61154426efdc8bca7379636
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-05-08 14:26:17 +0200

    #77431 clearing webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -22,35 +22,3 @@ Encore
 	});
 
 module.exports = Encore.getWebpackConfig();
-
-
-// var Encore = require('@symfony/webpack-encore');
-
-// Encore
-// 	.setOutputPath('web/build/')
-// 	//.setPublicPath('/build')
-// 	.setPublicPath('/build') //aktywny_dyrektor\web\build
-// 	//.cleanupOutputBeforeBuild() //new
-// 	//.setManifestKeyPrefix('/build') //new
-// 	.addEntry('app', './web/assets/js/app.js')
-// 	.enableSingleRuntimeChunk()
-// 	//.cleanupOutputBeforeBuild()
-// 	.enableSourceMaps(!Encore.isProduction())
-// 	.enableVersioning(Encore.isProduction())
-// 	.splitEntryChunks()
-// 	.autoProvidejQuery()
-// 	.enableSassLoader();
-
-// var config = Encore.getWebpackConfig();
-
-// config.devServer = {
-// 	//poll: true,
-// 	//ignored: /node_modules/,
-// 	//inline: true,
-// 	contentBase: './build', //   ./
-// 	port: 8009,
-// 	historyApiFallback: true
-// };
-
-
-// module.exports = config;
