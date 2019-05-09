397014e4226c59b232a81c26c77e1ec521520019
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-05-09 10:52:15 +0200

    #78346 renaming web/assets to web/src & and changing entry to web/src

---
+++ web/src/js/app.js
@@ -0,0 +1,10 @@
+import '../scss/app.scss';
+
+function showInfo(info) {
+    console.info(
+        `%c ${info} `,
+        'text-shadow: 2px 1px 0 #7A7A7A; color: #fff; font-size: 18px;',
+    );
+}
+
+showInfo();
