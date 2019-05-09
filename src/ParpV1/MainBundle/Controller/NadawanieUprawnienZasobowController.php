45aef0071b1d67393ca9e1104e3898d96036cf9f
Konrad Szelepusta <konrad_szelepusta@parp.gov.pl>
2019-05-09 09:38:18 +0200

    naprawia #79989

--- src/ParpV1/MainBundle/Controller/NadawanieUprawnienZasobowController.php
+++ src/ParpV1/MainBundle/Controller/NadawanieUprawnienZasobowController.php
@@ -479,13 +479,16 @@ class NadawanieUprawnienZasobowController extends Controller
                             $userZasob = $this->getDoctrine()->getManager()->getRepository(UserZasoby::class)->find($ps[0]);
                             $ids[] = $userZasob->getZasobId();
                             if (!in_array($userZasob->getZasobOpis(), $zasobyName)) {
-                                $zasobyName = $userZasob->getZasobOpis();
+                                $zasobyName[] = $userZasob->getZasobOpis();
                             }
                         }
 
                         $wniosek = $this->getDoctrine()->getRepository(WniosekNadanieOdebranieZasobow::class)->find($ndata['wniosekId']);
-                        $wniosek->setZasoby(implode(',', $zasobyName));
-                        $this->getDoctrine()->getManager()->persist($wniosek);
+                        if (null !== $wniosek) {
+                            $wniosek->setZasoby(implode(',', $zasobyName));
+                            $this->getDoctrine()->getManager()->persist($wniosek);
+                        }
+
                         $this->sprawdzCzyMozeDodawacOdbieracUprawnieniaBezWniosku($ids);
                     }
 
