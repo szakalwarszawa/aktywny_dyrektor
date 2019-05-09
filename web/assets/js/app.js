14b651e020700133892be2948d430e8699c7a621
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-05-09 09:12:43 +0200

    #78346 changing require to ES6 import standard

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -3,30 +3,3 @@ require('../scss/app.scss');
 // var $ = require('jquery');
 
 console.info("%c Hello from Encore ", "text-shadow: 2px 1px 0 #7A7A7A; color: #fff; font-size: 18px;");
-
-// const tab = [1, 2, 6, 4, 5];
-
-// for (let i = 0, sorted; i < 9; i++) {
-// 	let width = 0;
-// 	if (tab.includes(7)) {
-// 		(() => {
-// 			sorted = tab.map(item => item * 2 + i);
-// 			console.log(i, "Babel test: ", sorted);
-// 			return sorted;
-// 		})();
-// 	} else {
-// 		(() => {
-// 			class Prostokat {
-// 				constructor(wysokosc, szerokosc) {
-// 					this.wysokosc = wysokosc;
-// 					this.szerokosc = szerokosc;
-// 				}
-// 			}
-// 			const p = new Prostokat(5, 77);
-// 			let width = p.szerokosc;
-// 			return width;
-// 		})();
-// 	}
-
-// 	console.info(`${i} sorting list ${sorted} o szerokości ${width}`);
-// }
