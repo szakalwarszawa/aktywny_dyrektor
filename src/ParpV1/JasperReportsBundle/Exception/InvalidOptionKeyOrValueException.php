eeeba4cfbab4289b597de7a052a684f240c364a2
Konrad Szelepusta <konrad_szelepusta@parp.gov.pl>
2019-07-04 12:23:43 +0200

    missing class

---
+++ src/ParpV1/JasperReportsBundle/Exception/InvalidOptionKeyOrValueException.php
@@ -0,0 +1,11 @@
+<?php
+namespace ParpV1\JasperReportsBundle\Exception;
+
+use Exception;
+
+/**
+ * InvalidOptionKeyOrValueException
+ */
+class InvalidOptionKeyOrValueException extends Exception
+{
+}
