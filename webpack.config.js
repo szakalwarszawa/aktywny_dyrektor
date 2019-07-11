0171a7cd85933ee4d43bd9c42753fd17fa30e32d
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-07-11 10:09:25 +0200

    #85268 without .configureBabel for AWX test reason

--- webpack.config.js
+++ webpack.config.js
@@ -10,22 +10,22 @@ Encore.setOutputPath('web/build/')
     .enableVersioning(Encore.isProduction())
     .splitEntryChunks()
     .autoProvidejQuery()
-    .enableSassLoader()
-    // .enablePostCssLoader()
-    // .configureBabel(function(babelConfig) {
-    //     const preset = babelConfig.presets.find(
-    //         ([name]) => name === '@babel/preset-env',
-    //     );
-    //     if (preset !== undefined) {
-    //         preset[1].useBuiltIns = 'usage';
-    //         preset[1].corejs = '2.0.0';
-    //         preset[1].debug = true;
-    //     }
-    // })
-    .configureBabel(() => {}, {
-        useBuiltIns: 'usage',
-        corejs: { version: '3.1' },
-    });
+    .enableSassLoader();
+// .enablePostCssLoader()
+// .configureBabel(function(babelConfig) {
+//     const preset = babelConfig.presets.find(
+//         ([name]) => name === '@babel/preset-env',
+//     );
+//     if (preset !== undefined) {
+//         preset[1].useBuiltIns = 'usage';
+//         preset[1].corejs = '2.0.0';
+//         preset[1].debug = true;
+//     }
+// })
+// .configureBabel(() => {}, {
+//     useBuiltIns: 'usage',
+//     corejs: { version: '3.1.4' },
+// });
 
 if (Encore.isProduction()) {
     // Enable post css loader
