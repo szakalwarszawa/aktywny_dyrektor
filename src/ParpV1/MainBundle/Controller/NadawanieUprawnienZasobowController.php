88c549661f73d6dd5b2d4f22eeb54b2a2d4510f4
Konrad Szelepusta <konrad_szelepusta@parp.gov.pl>
2019-07-19 09:56:50 +0200

    naprawia #47128

--- src/ParpV1/MainBundle/Controller/NadawanieUprawnienZasobowController.php
+++ src/ParpV1/MainBundle/Controller/NadawanieUprawnienZasobowController.php
@@ -932,7 +932,8 @@ class NadawanieUprawnienZasobowController extends Controller
                         $statusWniosku = $wniosek->getWniosek()->getStatus()->getNazwaSystemowa();
                         $statusyMozliwaEdycjaCelu = [
                             '02_EDYCJA_PRZELOZONY',
-                            '00_TWORZONY'
+                            '00_TWORZONY',
+                            '01_EDYCJA_WNIOSKODAWCA'
                         ];
                         if ($oz->getId() > 0) {
                             //$z2 = $this->getDoctrine()->getManager()->getRepository(UserZasoby::class)->find($oz->getId());
