7232ea2851f0bd3461038dd75f6d907d56dcc0c3
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-08-01 15:52:26 +0200

    #85268 add import colresizable

--- web/src/js/app.js
+++ web/src/js/app.js
@@ -41,6 +41,7 @@ import '../../../node_modules/colresizable/colResizable-1.6.min.js';
 import * as datetimepicker from 'eonasdan-bootstrap-datetimepicker';
 import 'tablesorter';
 import 'tag-it';
+import 'colresizable';
 
 // require jQuery normally
 // const $ = require('jquery');
