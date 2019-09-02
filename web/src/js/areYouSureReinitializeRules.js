21b750eb33c61d5e5417ea7ae8639b287b282726
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-09-02 12:41:10 +0200

    #87963 test 2

--- web/src/js/areYouSureReinitializeRules.js
+++ web/src/js/areYouSureReinitializeRules.js
@@ -1,9 +1,10 @@
 const formsToReinitializeArr = ['#general form']; //tablica formularzy do ponownego zainicjowania po załadowaniu strony
 
 export default $(document).ready(function areYouSureReinitialize(formsToReinitializeArr) {
-    debugger;
     for (let i = 0; i < formsToReinitializeArr.length; i++) {
+        console.log('before', formsToReinitializeArr.length);
         let formToReinitialize = $(formsToReinitializeArr[i]);
+        console.log('after', formsToReinitializeArr.length);
         console.log('areYouSureReinitialize() loop ' + i + ' form ', formToReinitialize);
         if (formToReinitialize) {
             $(formToReinitialize).trigger('reinitialize.areYouSure');
