4b7524a89989194543862615dac45e1982e721f8
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-09-02 12:04:51 +0200

    #87963 enable reinitialization on many forms

--- web/src/js/areYouSureReinitializeRules.js
+++ web/src/js/areYouSureReinitializeRules.js
@@ -1,10 +1,14 @@
-export default $(document).ready(function areYouSureReinitialize() {
-    var generalForm = $('#general form');
-    console.log('areYouSureReinitialize() general form ', generalForm[0]);
-    if (generalForm) {
-        $(generalForm[0]).trigger('reinitialize.areYouSure');
-        console.info('Are You Sure reintialized');
-    } else {
-        console.warn('Are You Sure methods are not active here');
+const formsToReinitializeArr = ['#general form']; //tablica formularzy do ponownego zainicjowania po załadowaniu strony
+
+export default $(document).ready(function areYouSureReinitialize(formsToReinitializeArr) {
+    for (let i = 0; i < formsToReinitializeArr.length; i++) {
+        let formToReinitialize = $(formsToReinitializeArr[i]);
+        console.log('areYouSureReinitialize() loop ' + i + ' form ', formToReinitialize);
+        if (formToReinitialize) {
+            $(formToReinitialize).trigger('reinitialize.areYouSure');
+            console.info('Are You Sure reintialized');
+        } else {
+            console.warn('Are You Sure methods are not active here');
+        }
     }
 });
