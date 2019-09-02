bd13d7f0e71b44e1b7b4b0fbdca9eca7007b065b
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-09-02 13:37:31 +0200

    #87963 test 4 same function

--- web/src/js/areYouSureReinitializeRules.js
+++ web/src/js/areYouSureReinitializeRules.js
@@ -1,15 +1,18 @@
-const formsToReinitializeArr = ['#general form']; //tablica formularzy do ponownego zainicjowania po załadowaniu strony
+export default function areYouSureReinitialize() {
+    const formsToReinitializeArr = ['#general form']; //tablica formularzy do ponownego zainicjowania po załadowaniu strony
+    console.log('form ', formsToReinitializeArr, ' dł ', formsToReinitializeArr.length);
 
-export default $(document).ready(function areYouSureReinitialize(formsToReinitializeArr) {
-    console.log('el ', formsToReinitializeArr, ' dł ', formsToReinitializeArr.length);
-    for (let i = 0; i < formsToReinitializeArr.length; i++) {
-        let formToReinitialize = $(formsToReinitializeArr[i]);
-        console.log('areYouSureReinitialize() loop ' + i + ' form ', formToReinitialize);
-        if (formToReinitialize) {
-            $(formToReinitialize).trigger('reinitialize.areYouSure');
-            console.info('Are You Sure reintialized');
-        } else {
-            console.warn('There is no form to reinitialize');
+    $(document).ready(function(formsToReinitializeArr) {
+        console.log('el ', formsToReinitializeArr, ' dł ', formsToReinitializeArr.length);
+        for (let i = 0; i < formsToReinitializeArr.length; i++) {
+            let formToReinitialize = $(formsToReinitializeArr[i]);
+            console.log('areYouSureReinitialize() loop ' + i + ' form ', formToReinitialize);
+            if (formToReinitialize) {
+                $(formToReinitialize).trigger('reinitialize.areYouSure');
+                console.info('Are You Sure reintialized');
+            } else {
+                console.warn('There is no form to reinitialize');
+            }
         }
-    }
-});
+    });
+}
