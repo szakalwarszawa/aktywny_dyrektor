1e5cfd7fb23defbac969f5443267e27e49db0696
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-09-02 10:50:02 +0200

    #87963 reinitialize on ready

--- web/src/js/areYouSureReinitializeRules.js
+++ web/src/js/areYouSureReinitializeRules.js
@@ -1,10 +1,10 @@
-export default (function areYouSureReinitialize() {
+export default $(document).ready(function areYouSureReinitialize() {
     var generalForm = $('#general form');
-    console.log('general form ', generalForm[0]);
+    console.log('areYouSureReinitialize() general form ', generalForm[0]);
     if (generalForm) {
         $(generalForm[0]).trigger('reinitialize.areYouSure');
         console.info('Are You Sure reintialized');
     } else {
         console.warn('Are You Sure methods are not active here');
     }
-})();
+});
