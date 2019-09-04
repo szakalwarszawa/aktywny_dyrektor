d1cebf6c4f2059ecba13235d5ad9480a064155ae
Szymon Wojcikowski <szymon_wojcikowski@parp.gov.pl>
2019-09-04 14:15:15 +0200

    #88885 init on this el

--- web/src/js/dateTimePickerSetter.js
+++ web/src/js/dateTimePickerSetter.js
@@ -1,13 +1,13 @@
 export default $(function dateTimePickerSetter() {
     $('.datetimepicker:not([disabled])').on('click', function() {
-        $('.datetimepicker:not([disabled])').datetimepicker({
+        $(this).datetimepicker({
             inline: true,
             locale: 'pl',
             format: 'YYYY-MM-DD HH:mm',
         })
     });
     $('.datetimepicker:not([disabled])').on('focus', function() {
-        $('.datetimepicker:not([disabled])').datetimepicker({
+        $(this).datetimepicker({
             inline: true,
             locale: 'pl',
             format: 'YYYY-MM-DD HH:mm',
